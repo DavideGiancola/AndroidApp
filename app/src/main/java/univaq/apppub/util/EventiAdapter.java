4fb21ce7e6ba8eac89d9358ec09752d888871812
Davide Giancola <gia.davide@gmail.com>
2017-12-10 19:24:07 +0100

    Primi aggiustamenti lista eventi

--- app/src/main/java/univaq/apppub/util/EventiAdapter.java
+++ app/src/main/java/univaq/apppub/util/EventiAdapter.java
@@ -1,11 +1,15 @@
 package univaq.apppub.util;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
 
 import java.util.List;
 
@@ -18,12 +22,13 @@ import univaq.apppub.model.Evento;
 
 public class EventiAdapter extends RecyclerView.Adapter<EventiAdapter.ViewHolder> {
 
-
+    private Context mContext;
     private List<Evento> mEventi;
 
 
 
-    public EventiAdapter(List<Evento> eventi) {
+    public EventiAdapter(Context mContext, List<Evento> eventi) {
+        this.mContext = mContext;
         mEventi = eventi;
     }
 
@@ -39,8 +44,9 @@ public class EventiAdapter extends RecyclerView.Adapter<EventiAdapter.ViewHolder
         holder.nome.setText(mEventi.get(position).getNome());
         holder.data.setText(mEventi.get(position).getData());
         holder.oraInizio.setText(mEventi.get(position).getOraInizio());
-        holder.oraFine.setText(mEventi.get(position).getOraFine());
-        holder.descrizione.setText(mEventi.get(position).getDescrizione());
+        //holder.oraFine.setText(mEventi.get(position).getOraFine());
+        //holder.descrizione.setText(mEventi.get(position).getDescrizione());
+        Glide.with(mContext).load(mEventi.get(position).getImg()).into(holder.img);
     }
 
     @Override
@@ -51,6 +57,7 @@ public class EventiAdapter extends RecyclerView.Adapter<EventiAdapter.ViewHolder
     public static class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
 
         TextView nome, data, oraInizio, oraFine, descrizione;
+        ImageView img;
 
         public ViewHolder(View view) {
             super(view);
@@ -58,8 +65,9 @@ public class EventiAdapter extends RecyclerView.Adapter<EventiAdapter.ViewHolder
             nome = (TextView) view.findViewById(R.id.customadapter_text_nome);
             data = (TextView) view.findViewById(R.id.customadapter_text_data);
             oraInizio = (TextView) view.findViewById(R.id.customadapter_text_ora_inizio);
-            oraFine = (TextView) view.findViewById(R.id.customadapter_text_ora_fine);
-            descrizione = (TextView) view.findViewById(R.id.customadapter_text_descrizione);
+            //oraFine = (TextView) view.findViewById(R.id.customadapter_text_ora_fine);
+            //descrizione = (TextView) view.findViewById(R.id.customadapter_text_descrizione);
+            img = (ImageView) view.findViewById(R.id.imamagineEvento);
 
             view.setOnClickListener(this);
         }
